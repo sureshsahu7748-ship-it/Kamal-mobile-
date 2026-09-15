44a462516bf148ed93c1e7efe4398a926355a228
sureshsahu7748-ship-it <sureshsahu7748@gmail.com>
2026-09-15 08:31:36 +0530

    Add Hindi comments to firebase-config.js
    Added comments in Hindi for configuration clarity.

--- firebase-config.js
+++ firebase-config.js
@@ -2,6 +2,7 @@
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
+// फ़ायरबेस प्रोजेक्ट कॉन्फ़िगरेशन
 const firebaseConfig = {
   apiKey: "AIzaSyAIGqg5dmdDVAzv7x7sMDOfUcxj6iN3Ibw",
   authDomain: "kamal-mobile-d8ba5.firebaseapp.com",
@@ -11,9 +12,11 @@ const firebaseConfig = {
   appId: "1:1065271731721:web:e4163af79a3609a50cbad8"
 };
 
+// आपकी नई UID
 export const OWNER_UID = "sNvcKVBpCHaxtYj2LpEAsHrqIsq1";
 export const OWNER_UIDS = [OWNER_UID];
 
+// फ़ायरबेस ऐप्स और सर्विसेज़ इनिशियलाइज़ेशन
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
